8b44f27fed3478bbb2a8bdb0c914946294ac9080
EktorGG <profesorhectorg@gmail.com>
2022-09-16 00:19:02 -0400

    asda

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     //Similar a querySelectorAll()
-    $("#cabecera").load("ektorgg.github.io\html\parts\header.html");
-    $("#pie-pagina").load("ektorgg.github.io\html\parts\footer.html");
+    $("#cabecera").load("html/parts/header.html");
+    $("#pie-pagina").load("html/parts/gifooter.html");
 });
