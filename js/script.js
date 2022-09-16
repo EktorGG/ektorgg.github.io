2875ee1a9664de5878d98ff74d0a9b2d029eac06
EktorGG <profesorhectorg@gmail.com>
2022-09-16 00:24:57 -0400

    about y contact

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     //Similar a querySelectorAll()
-    $("#cabecera").load("html/parts/header.html");
-    $("#pie-pagina").load("html/parts/gifooter.html");
+    $("#cabecera").load("/html/parts/header.html");
+    $("#pie-pagina").load("/html/parts/gifooter.html");
 });
