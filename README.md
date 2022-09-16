76ed6e97ee595972f116e56e805a4d74f94814f4
EktorGG <profesorhectorg@gmail.com>
2022-09-15 22:04:32 -0400

    intentando hacer cambios importantes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# FrontSMU
-## Proyecto de portafolio para el curso de front de AIEP y SMU
+# Entrega final proyecto portafolio para curso front end SMU
+
+## Para este proyecto se utilizó html, css y javascript en su mayoría, puede contener algunos elementos en bootstrap y react.
+## La información enviada a través de los formularios queda en Firebase.
+## La página web andando debería poder visualizarse correctamente en https://ektorgg.github.io/portaflioEktorGG.github.io/
--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //Similar a querySelectorAll()
-    $("#cabecera").load("/html/parts/header.html");
-    $("#pie-pagina").load("/html/parts/footer.html");
-
-    $("btn-contacto").on("click", function(){
-        const email = $("#correo").val();
-        const mensaje = $("mensaje").val();
-        console.dir({email, mensaje});
-    })
+    $("#cabecera").load("./html/parts/header.html");
+    $("#pie-pagina").load("./html/parts/footer.html");
 });
